0f3c91426fab19a1c71652f9544d8be3f14b1800
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-05-08 18:17:05 -0400

    Listo el menu movil.

---
+++ src/scripts/toogle.js
@@ -0,0 +1,29 @@
+// Variables
+const ToogleIcon = document.getElementById('toogle__icon');
+const NavMobile = document.getElementById('nav-mobile');
+const ToogleIconClose = document.getElementById('toogle-close');
+const MobileLinks = document.querySelectorAll('.navbar-mobile__link');
+
+// EventListeners
+ToogleIcon.addEventListener('click', () => {
+    OpenToggle();
+});
+
+ToogleIconClose.addEventListener('click', () => {
+    CloseToggle();
+});
+
+MobileLinks.forEach((e) => {
+    e.addEventListener('click', () => {
+        CloseToggle();
+    });
+});
+
+// Functions
+const OpenToggle = () => {
+    NavMobile.classList.add('nav-mobile--open');
+};
+
+const CloseToggle = () => {
+    NavMobile.classList.remove('nav-mobile--open');
+};
