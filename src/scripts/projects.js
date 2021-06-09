93567dd2a95e7efce93cbd308f2c0d7b657786f4
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-06-08 22:58:05 -0400

    No usare webpack en este proyecto, se cambiaron las imagenes de png a webp

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,140 +1,140 @@
 const Projects = [
-    {
-        id: 'valorant',
-        name: 'Valorant',
-        image: '/src/images/projects/valorant.png',
-        font: {
-            title: "'Druk Wide Medium', sans-serif",
-            body: "'Montserrat', sans-serif",
-        },
-        colors: {
-            primary: '#ffff',
-            alt: '#ff4656',
-            background: '#1f2326',
-        },
-        tech: [
-            {
-                name: 'HTML',
-                image: '/src/images/techs/html5.svg',
-            },
-            {
-                name: 'CSS',
-                image: '/src/images/techs/css3-alt.svg',
-            },
-            {
-                name: 'React',
-                image: '/src/images/techs/react.svg',
-            },
-            {
-                name: 'Sass',
-                image: '/src/images/techs/sass.svg',
-            },
-        ],
-        link: 'https://jrs-developer.github.io/valorant/',
-    },
-    {
-        id: 'rick',
-        name: 'Rick-&-Morty',
-        image: '/src/images/projects/Rick&Morty.png',
-        font: {
-            title: "'Poppins', sans-serif",
-            body: "'Poppins', sans-serif",
-        },
-        colors: {
-            primary: '#ffff',
-            alt: '#00B0C8',
-            background: '#290931',
-        },
-        tech: [
-            {
-                name: 'HTML',
-                image: '/src/images/techs/html5.svg',
-            },
-            {
-                name: 'CSS',
-                image: '/src/images/techs/css3-alt.svg',
-            },
-            {
-                name: 'JavaScript',
-                image: '/src/images/techs/js.svg',
-            },
-            {
-                name: 'Sass',
-                image: '/src/images/techs/sass.svg',
-            },
-        ],
-        link: 'https://jrs-developer.github.io/rick-morty/',
-    },
-    {
-        id: 'cart',
-        name: 'Shopping-Cart',
-        image: '/src/images/projects/shopping-cart.png',
-        font: {
-            title: "'Rubik', sans-serif",
-            body: "'Rubik', sans-serif",
-        },
-        colors: {
-            primary: '#f8f9fa',
-            alt: '#d90429',
-            background: '#212529',
-        },
-        tech: [
-            {
-                name: 'HTML',
-                image: '/src/images/techs/html5.svg',
-            },
-            {
-                name: 'CSS',
-                image: '/src/images/techs/css3-alt.svg',
-            },
-            {
-                name: 'JavaScript',
-                image: '/src/images/techs/js.svg',
-            },
-            {
-                name: 'Sass',
-                image: '/src/images/techs/sass.svg',
-            },
-        ],
-        link: 'https://jrs-developer.github.io/shopping-cart/',
-    },
-    {
-        id: 'commerce',
-        name: 'E-Commerce',
-        image: '/src/images/projects/E-commerce.png',
-        font: {
-            title: "'Poppins', sans-serif",
-            body: "'Poppins', sans-serif",
-        },
-        colors: {
-            primary: '#f8f9fa',
-            alt: '#d90429',
-            background: '#001427',
-        },
-        tech: [
-            {
-                name: 'HTML',
-                image: '/src/images/techs/html5.svg',
-            },
-            {
-                name: 'CSS',
-                image: '/src/images/techs/css3-alt.svg',
-            },
-            {
-                name: 'JavaScript',
-                image: '/src/images/techs/js.svg',
-            },
-            {
-                name: 'Sass',
-                image: '/src/images/techs/sass.svg',
-            },
-            {
-                name: 'Sass',
-                image: '/src/images/techs/php.svg',
-            },
-        ],
-        link: 'https://online-shop-jrs.000webhostapp.com/',
-    },
+	{
+		id: 'valorant',
+		name: 'Valorant',
+		image: '/src/images/projects/valorant.webp',
+		font: {
+			title: "'Druk Wide Medium', sans-serif",
+			body: "'Montserrat', sans-serif",
+		},
+		colors: {
+			primary: '#ffff',
+			alt: '#ff4656',
+			background: '#1f2326',
+		},
+		tech: [
+			{
+				name: 'HTML',
+				image: '/src/images/techs/html5.svg',
+			},
+			{
+				name: 'CSS',
+				image: '/src/images/techs/css3-alt.svg',
+			},
+			{
+				name: 'React',
+				image: '/src/images/techs/react.svg',
+			},
+			{
+				name: 'Sass',
+				image: '/src/images/techs/sass.svg',
+			},
+		],
+		link: 'https://jrs-developer.github.io/valorant/',
+	},
+	{
+		id: 'rick',
+		name: 'Rick-&-Morty',
+		image: '/src/images/projects/Rick_Morty.webp',
+		font: {
+			title: "'Poppins', sans-serif",
+			body: "'Poppins', sans-serif",
+		},
+		colors: {
+			primary: '#ffff',
+			alt: '#00B0C8',
+			background: '#290931',
+		},
+		tech: [
+			{
+				name: 'HTML',
+				image: '/src/images/techs/html5.svg',
+			},
+			{
+				name: 'CSS',
+				image: '/src/images/techs/css3-alt.svg',
+			},
+			{
+				name: 'JavaScript',
+				image: '/src/images/techs/js.svg',
+			},
+			{
+				name: 'Sass',
+				image: '/src/images/techs/sass.svg',
+			},
+		],
+		link: 'https://jrs-developer.github.io/rick-morty/',
+	},
+	{
+		id: 'cart',
+		name: 'Shopping-Cart',
+		image: '/src/images/projects/shopping-cart.webp',
+		font: {
+			title: "'Rubik', sans-serif",
+			body: "'Rubik', sans-serif",
+		},
+		colors: {
+			primary: '#f8f9fa',
+			alt: '#d90429',
+			background: '#212529',
+		},
+		tech: [
+			{
+				name: 'HTML',
+				image: '/src/images/techs/html5.svg',
+			},
+			{
+				name: 'CSS',
+				image: '/src/images/techs/css3-alt.svg',
+			},
+			{
+				name: 'JavaScript',
+				image: '/src/images/techs/js.svg',
+			},
+			{
+				name: 'Sass',
+				image: '/src/images/techs/sass.svg',
+			},
+		],
+		link: 'https://jrs-developer.github.io/shopping-cart/',
+	},
+	{
+		id: 'commerce',
+		name: 'E-Commerce',
+		image: '/src/images/projects/E-commerce.webp',
+		font: {
+			title: "'Poppins', sans-serif",
+			body: "'Poppins', sans-serif",
+		},
+		colors: {
+			primary: '#f8f9fa',
+			alt: '#d90429',
+			background: '#001427',
+		},
+		tech: [
+			{
+				name: 'HTML',
+				image: '/src/images/techs/html5.svg',
+			},
+			{
+				name: 'CSS',
+				image: '/src/images/techs/css3-alt.svg',
+			},
+			{
+				name: 'JavaScript',
+				image: '/src/images/techs/js.svg',
+			},
+			{
+				name: 'Sass',
+				image: '/src/images/techs/sass.svg',
+			},
+			{
+				name: 'Sass',
+				image: '/src/images/techs/php.svg',
+			},
+		],
+		link: 'https://online-shop-jrs.000webhostapp.com/',
+	},
 ];
 
 export default Projects;
