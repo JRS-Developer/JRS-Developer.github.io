d9930795da42e7a74594287a8c421b8a0b45d9be
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-07-28 15:23:42 -0400

    feat: added a mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
 import { socialLinks } from "@/data/socialLinks";
-import { Box, Flex, Heading, Icon, Link, Spacer, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useDisclosure,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
-import { NextRouter, useRouter, withRouter } from "next/router";
-import { MouseEvent } from "react";
+import { NextRouter, withRouter } from "next/router";
+import { HiMenu, HiX } from "react-icons/hi";
 
 interface LinkT {
   title: string;
@@ -28,11 +40,24 @@ interface Props {
   router: NextRouter;
 }
 
+interface DesktopNavProps {
+  path: string;
+}
+
 function Navbar({ router: { asPath } }: Props) {
   const path = asPath.replace("/", "");
 
   return (
-    <Flex as="nav" px="16" alignItems="center" h="100%">
+    <Flex
+      as="nav"
+      px={["8", "16"]}
+      alignItems="center"
+      pos="relative"
+      h="100%"
+      justifyContent="space-between"
+      borderBottom="1px"
+      borderColor={["gray.500", "none"]}
+    >
       <NextLink href="/" passHref>
         <Link
           _hover={{
@@ -45,47 +70,77 @@ function Navbar({ router: { asPath } }: Props) {
         </Link>
       </NextLink>
 
-      <Spacer />
+      <DesktopNav path={path} />
 
-      <Flex gap="8">
-        {navLinks.map(({ link, title }) => (
-          <NextLink key={title} href={link} passHref>
-            <Link
-              fontWeight="medium"
-              rounded="md"
-              _active={{
-                textColor: "gray.900",
-              }}
-              _hover={{
-                textDecor: "none",
-                textColor: "gray.900",
-              }}
-              textColor={`${link === path ? "gray.900" : "gray.400"}`}
-            >
-              {title}
-            </Link>
-          </NextLink>
-        ))}
-      </Flex>
-
-      <Flex ml="12" alignItems="center" gap="4">
-        {socialLinks.map(({ icon, label, link }) => (
-          <NextLink href={link} key={label} passHref>
-            <Link
-              display="flex"
-              alignItems="center"
-              isExternal
-              _hover={{
-                opacity: 0.75,
-              }}
-            >
-              <Icon as={icon} h={4} w={4} />
-            </Link>
-          </NextLink>
-        ))}
-      </Flex>
+      <MobileNav />
     </Flex>
   );
 }
+
+const DesktopNav = ({ path }: DesktopNavProps) => (
+  <Box display={["none", "flex"]}>
+    <Flex gap="8">
+      {navLinks.map(({ link, title }) => (
+        <NextLink key={title} href={link} passHref replace={true}>
+          <Link
+            fontWeight="medium"
+            rounded="md"
+            _active={{
+              textColor: "gray.900",
+            }}
+            _hover={{
+              textDecor: "none",
+              textColor: "gray.900",
+            }}
+            textColor={`${link === path ? "gray.900" : "gray.400"}`}
+          >
+            {title}
+          </Link>
+        </NextLink>
+      ))}
+    </Flex>
+
+    <Flex ml="12" alignItems="center" gap="4">
+      {socialLinks.map(({ icon, label, link }) => (
+        <NextLink href={link} key={label} passHref>
+          <Link
+            display="flex"
+            alignItems="center"
+            isExternal
+            _hover={{
+              opacity: 0.75,
+            }}
+          >
+            <Icon as={icon} h={4} w={4} />
+          </Link>
+        </NextLink>
+      ))}
+    </Flex>
+  </Box>
+);
+
+const MobileNav = () => {
+  return (
+    <Menu isLazy>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            display={["flex", "none"]}
+            aria-label="Toggle Menu"
+            as={IconButton}
+            icon={isOpen ? <HiX /> : <HiMenu />}
+          />
+          <MenuList>
+            {navLinks.map(({ title, link }) => (
+              <NextLink href={link} passHref key={`mobile-${title}`}>
+                <MenuItem as="a">{title}</MenuItem>
+              </NextLink>
+            ))}
+          </MenuList>
+        </>
+      )}
+    </Menu>
+  );
+};
 
 export default withRouter(Navbar);
